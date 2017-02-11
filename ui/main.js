3b3eeac48df3f834f50c7c647af87ee087cd13ae
arv-raj <arajan136@gmail.com>
2017-02-11 23:30:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+
+var img = document.getElementById('madi');
+var marginLeft = o;
+function moveRight() {
+    var marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function () {
+    var interval = setInterval(moveRight, 50);
+};
