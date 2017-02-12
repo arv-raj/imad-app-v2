343fe14dcbcd133262860193aa72a7eb585a40aa
arv-raj <arajan136@gmail.com>
2017-02-12 13:08:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,8 @@ app.get('/counter', function (req, res){
 
 var names =[];
 app.get('/submit-name', function (req, res){
-    names = req.query.names;
+    var name = req.query.name;
+    names.push(name);
     res.send(JSON.stringify(names));
 });
 
