ca5f70bd6f5b08eabcc0fd11b3748f7e17a8764a
arv-raj <arajan136@gmail.com>
2017-02-12 12:09:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,12 +73,6 @@ function createTemplate (data){
     return htmlTemplate;
 }    
 
-var counter =0;
-app.get('/counter', function (req, res){
-    counter = counter + 1;
-    res.send(counter.toString());
-});
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -100,6 +94,11 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var counter =0;
+app.get('/counter', function (req, res){
+    counter = counter + 1;
+    res.send(counter.toString());
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
