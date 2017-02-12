c8b8dae806d47b413ae318c1e5072067b289bbaa
arv-raj <arajan136@gmail.com>
2017-02-12 12:56:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,6 +87,12 @@ app.get('/counter', function (req, res){
     res.send(counter.toString());
 });
 
+var names =[];
+app.get('submit-name', function (req, res){
+    names = req.query.names;
+    res.send(JSON.stringify(names));
+});
+
 app.get('/:articleName', function(req, res) {
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName])); 
